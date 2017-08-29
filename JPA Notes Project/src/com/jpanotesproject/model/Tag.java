1c17ce05f60114c9476e1845a44c14cdf879a5a5
Maximetinu <maximetinu@gmail.com>
2017-08-29 21:24:01 +0200

    Tag corrected

--- JPA Notes Project/src/com/jpanotesproject/model/Tag.java
+++ JPA Notes Project/src/com/jpanotesproject/model/Tag.java
@@ -1,108 +1,29 @@
 package com.jpanotesproject.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-//import java.util.HashMap;
-
 import javax.persistence.Column;
-//import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-//import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "NOTE")
-public /* abstract */ class Note extends BaseEntity {
+@Table
+public class Tag extends BaseEntity{
 
-	@Column(name = "TITLE")
-	private String title;
-	@Column(name = "CREATION_DATE")
-	private java.sql.Timestamp creationDate;
-	@Column(name = "LAST_EDIT_DATE")
-	private java.sql.Timestamp lastEditDate;;
-
-	//@ManyToOne
-	//@Column(name = "AUTHOR")
-	//private User author;
-
-	//@ElementCollection
-	//@Column(name = "SHARED_USERS")
-	//private HashMap<User, Integer> sharedUsers;
+	@Column(unique = true)
+	private String tagText;
 	
-	@ManyToMany
-	private List<Tag> tags;
-
-	public Note(/*User author,*/ String title) {
-		super();
-		//this.author = author;
-		this.title = title;
-		
-		long now = new java.util.Date().getTime();
-		this.creationDate = new java.sql.Timestamp(now);
-		this.lastEditDate = creationDate;
-		
-		tags = new ArrayList<Tag>();
-		
-		//sharedUsers = new HashMap<User, Integer>();
-	}
-	
-	public void addTag(Tag t) {
-		tags.add(t);
-	}
-	
-	public void removeTag(Tag t) {
-		tags.remove(t);
-	}
-	
-	public List<Tag> getTagsList(){
-		return tags;
-	}
-
-	public Note() {
+	public Tag() {
 		super();
 	}
 	
-	/*public boolean canRead(User user) {
-		return sharedUsers.containsKey(user) && sharedUsers.get(user) >= 1;
-	}
-
-	public boolean canReadAndWrite(User user) {
-		return sharedUsers.containsKey(user) && sharedUsers.get(user) == 2;
+	public Tag(String tagText) {
+		this.tagText = tagText;
 	}
 	
-	public void setPermission(User user, Integer permissionLevel) {
-		if (permissionLevel > 0)
-			sharedUsers.put(user, permissionLevel);
-		else
-			sharedUsers.remove(user);
-	}*/
-
-	public java.sql.Timestamp getCreationDate() {
-		return creationDate;
+	public String getTagText() {
+		return tagText;
 	}
 	
-	private void updateLastEditDate() {
-		long now = new java.util.Date().getTime();
-		this.lastEditDate = new java.sql.Timestamp(now);
-	}
-	
-	public java.sql.Timestamp getLastEditDate(){
-		return lastEditDate;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-		this.updateLastEditDate();
-	}
-
-	@Override
-	public String toString() {
-		return "Note [creationDate=" + creationDate + ", title=" + title + "]";
+	public void setTagText(String tagText) {
+		this.tagText = tagText;
 	}
 }
