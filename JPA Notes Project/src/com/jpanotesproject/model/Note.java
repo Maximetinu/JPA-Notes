66890ea85169c46af5b34105047e123e574ecf4f
Maximetinu <maximetinu@gmail.com>
2017-08-29 18:35:42 +0200

    Note and User almost finished

--- JPA Notes Project/src/com/jpanotesproject/model/Note.java
+++ JPA Notes Project/src/com/jpanotesproject/model/Note.java
@@ -1,84 +1,71 @@
 package com.jpanotesproject.model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-/*
- * We still lack the BaseEntity interface, the Notes subclasses, the Tags and Users entities and relationships with Note, etc. This class has been built with testing purposes
- *///
+
 @Entity
 @Table
-public abstract class Note extends BaseEntity{
-   
-   private String title;
-   private String creationDate;
-   @OneToOne
-   private User author;
-   @OneToMany
-   private ArrayList<User> users_can_write;
-   @OneToMany
-   private ArrayList<User> users_can_read;
-   
-   
-   public Note(User author, String creation_date, String title) {
-      super( );
-      this.author = author;
-      this.title = title;
-      
-      addPermissionWriteToUser(author);
-      addPermissionReadToUser(author);
-      setCreationDate(creation_date);
-   }
-   
-   public Note( ) {
-      super();
-   }
-   
-   public void addPermissionWriteToUser(User user) {
-	   users_can_write.add(user);
-   }
-   
-   public void addPermissionReadToUser(User user) {
-	   users_can_read.add(user);
-   } 
+public /* abstract */ class Note extends BaseEntity {
+	
+	private String title;
+	private String creationDate;
 
-   public void removePermissionWriteToUser(User user) {
-	   users_can_write.remove(user);
-   }
+	@ManyToOne
+	private User author;
 
-   public void removePermissionReadToUser(User user) {
-	   users_can_read.remove(user);
-   }
-   
-   public boolean isPossibleRead(User user) {
-	   return users_can_read.contains(user);
-   }
-   
-   public boolean isPossibleWrite(User user) {
-	   return users_can_write.contains(user);
-   }
-   
-   public String getCreationDate( ) {
-      return creationDate;
-   }
-   
-   private void setCreationDate(String creationDate) {
-      this.creationDate = creationDate;
-   }
-   
-   public String getTitle( ) {
-      return title;
-   }
-   
-   public void setTitle(String title) {
-      this.title = title;
-   }
-   
-   @Override
-   public String toString() {
-      return "Note [creationDate=" + creationDate + ", title=" + title + "]";
-   }
+	@ElementCollection
+	private HashMap<User, Integer> sharedUsers;
+
+	public Note(User author, String creation_date, String title) {
+		super();
+		this.author = author;
+		this.title = title;
+		creationDate = creation_date;
+		
+		sharedUsers = new HashMap<User, Integer>();
+	}
+
+	public Note() {
+		super();
+	}
+	
+	public boolean canRead(User user) {
+		return sharedUsers.containsKey(user) && sharedUsers.get(user) >= 1;
+	}
+
+	public boolean canReadAndWrite(User user) {
+		return sharedUsers.containsKey(user) && sharedUsers.get(user) == 2;
+	}
+	
+	public void setPermission(User user, Integer permissionLevel) {
+		if (permissionLevel > 0)
+			sharedUsers.put(user, permissionLevel);
+		else
+			sharedUsers.remove(user);
+	}
+
+	public String getCreationDate() {
+		return creationDate;
+	}
+
+	private void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Note [creationDate=" + creationDate + ", title=" + title + "]";
+	}
 }
--- JPA Notes Project/src/com/jpanotesproject/model/User.java
+++ JPA Notes Project/src/com/jpanotesproject/model/User.java
@@ -1,37 +1,62 @@
 package com.jpanotesproject.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table
-public class User extends BaseEntity{
+public class User extends BaseEntity {
 
-	   @Column(unique=true) 
-	   private String name;
-	   
-	   private String password;
-	   private String email;
-	   private String registration_date;
-	   
-	   private ArrayList<Note> notes;
-	   
-	   public User(String name, String password, String email, String registration_date) {
-	      super( );
-	      this.name = name;
-	      this.password = password;
-	      this.email = email;
-	      this.registration_date = registration_date;
-	   }
-	   
-	   public User() {
-		      super();
-	   }
-	   
-	   public void addNote(Note note) {
-		   notes.add(note);
-	   }
+	@Column(unique = true)
+	private String username;
+
+	private String password;
+	private String email;
+	private String registration_date;
+
+	@ElementCollection
+	private HashMap<Note, Integer> sharedNotes;
+
+	@OneToMany
+	private ArrayList<Note> ownNotes;
+
+	public User(String name, String password, String email, String registration_date) {
+		super();
+		this.username = name;
+		this.password = password;
+		this.email = email;
+		this.registration_date = registration_date;
+	}
+
+	public User() {
+		super();
+	}
+	
+	public boolean canRead(Note note) {
+		if (ownNotes.contains(note)) 
+			return true;
+		else
+			return sharedNotes.containsKey(note) && sharedNotes.get(note) >= 1;
+	}
+
+	public boolean canReadAndWrite(Note note) {
+		if (ownNotes.contains(note)) 
+			return true;
+		else
+			return sharedNotes.containsKey(note) && sharedNotes.get(note) == 2;
+	}
+	
+	public void setPermission(Note note, Integer permissionLevel) {
+		if (permissionLevel > 0)
+			sharedNotes.put(note, permissionLevel);
+		else
+			sharedNotes.remove(note);
+	}
 }
