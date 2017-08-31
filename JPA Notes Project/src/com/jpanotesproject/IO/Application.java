f29fe11efedcbb932de1a603eadafdf8ccd9908c
Maximetinu <maximetinu@gmail.com>
2017-08-31 22:59:58 +0200

    Main fixed, now it works properly

--- JPA Notes Project/src/com/jpanotesproject/IO/Application.java
+++ JPA Notes Project/src/com/jpanotesproject/IO/Application.java
@@ -4,8 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import com.jpanotesproject.daos.*;
-import com.jpanotesproject.model.*;
+import com.jpanotesproject.daos.TagDAO;
+import com.jpanotesproject.model.Tag;
 
 /*
  * This class is meant to be the interface of our application by managing user's input by a terminal menu (in this basic case)
@@ -15,21 +15,27 @@ import com.jpanotesproject.model.*;
 public class Application {
 
 	public static void main(String[] args) {
-		
-		// Tag entity working
-		// TODO: Uncomment from persistence.xml User and Note (one by one), debug and correct
-		
-	//	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "JPA Notes Project" );
-	  //  EntityManager em = emfactory.createEntityManager( );
-	   // em.getTransaction( ).begin( );
-		
-		NotesCRUDService tDAO = new NotesCRUDService();
-		tDAO.create();
-		
-	//	em.getTransaction( ).commit( );
 
-	//    em.close( );
-	  //  emfactory.close( );
+		// Tag entity working
+		// TODO: Uncomment from persistence.xml User and Note (one by one), debug and
+		// correct
+
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA Notes Project");
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+
+		// NotesCRUDService tDAO = new NotesCRUDService();
+		// tDAO.create();
+
+		Tag t = new Tag("haaa");
+		TagDAO tDAO = new TagDAO(em);
+		tDAO.persist(t);
+		// REALLY NIGGA
+
+		em.getTransaction().commit();
+
+		em.close();
+		emfactory.close();
 	}
 
 }
