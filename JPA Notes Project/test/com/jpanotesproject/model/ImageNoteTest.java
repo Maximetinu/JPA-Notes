d1c05bc78a83fbaf7a200b5e49f4dc9705de9307
Maximetinu <maximetinu@gmail.com>
2017-09-17 20:28:49 +0200

    ImageNoteText coded

---
+++ JPA Notes Project/test/com/jpanotesproject/model/ImageNoteTest.java
@@ -0,0 +1,61 @@
+package com.jpanotesproject.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jpanotesproject.helpers.AssertAnnotations;
+import com.jpanotesproject.helpers.ReflectTool;
+
+public class ImageNoteTest {
+
+	@Test
+	public void noteInheritance() {
+		Assert.assertEquals(Note.class, ImageNote.class.getSuperclass());
+	}
+
+	@Test
+	public void typeAnnotations() {
+		AssertAnnotations.assertType(ImageNote.class, Entity.class, Table.class);
+	}
+
+	@Test
+	public void fieldAnnotations() {
+		AssertAnnotations.assertField(ImageNote.class, "img", Column.class);
+	}
+
+	@Test
+	public void gettersAnnotations() {
+		AssertAnnotations.assertMethod(ImageNote.class, "getImage");
+	}
+
+	@Test
+	public void entity() {
+		// setup
+		Entity a = ReflectTool.getClassAnnotation(ImageNote.class, Entity.class);
+
+		// assert
+		Assert.assertEquals("", a.name());
+	}
+
+	@Test
+	public void table() {
+		// setup
+		Table a = ReflectTool.getClassAnnotation(ImageNote.class, Table.class);
+
+		// assert
+		Assert.assertEquals("Image_Note", a.name());
+	}
+
+	@Test
+	public void content() {
+		// setup
+		Column c = ReflectTool.getFieldAnnotation(ImageNote.class, "img", Column.class);
+
+		// assert
+		Assert.assertEquals("content", c.name());
+	}
+}
