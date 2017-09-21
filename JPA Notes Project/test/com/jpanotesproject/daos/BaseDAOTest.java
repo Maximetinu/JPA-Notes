7d4c7ade9800552530812023c3e7d5b64b01276a
Maximetinu <maximetinu@gmail.com>
2017-09-22 01:54:05 +0200

    BaseDAO and EntityManagerMock finished

--- JPA Notes Project/test/com/jpanotesproject/daos/BaseDAOTest.java
+++ JPA Notes Project/test/com/jpanotesproject/daos/BaseDAOTest.java
@@ -1,5 +1,76 @@
 package com.jpanotesproject.daos;
 
+import javax.persistence.EntityManager;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jpanotesproject.mocks.EntityManagerMock;
+import com.jpanotesproject.model.BaseEntity;
+
 public class BaseDAOTest {
+
+	// Setting Up Mocking structure
+
+	public class MockingEntity extends BaseEntity {
+		public MockingEntity() {
+			super();
+			super.id = null;
+		}
+
+		public MockingEntity(long id) {
+			super();
+			super.id = id;
+		}
+	}
+
+	public class MockingDAO extends BaseDAO<MockingEntity> {
+		public MockingDAO(EntityManager context) {
+			super(MockingEntity.class, context);
+		}
+
+	}
+
+	MockingDAO mockDAO;
+	EntityManagerMock em;
+
+	@Before
+	public void setUp() {
+		em = new EntityManagerMock();
+		mockDAO = new MockingDAO(em);
+	}
+
+	@Test
+	public void persistTest() {
+		MockingEntity entityMock = new MockingEntity();
+
+		mockDAO.persist(entityMock);
+		Assert.assertTrue(em.isPersistCalled());
+		Assert.assertFalse(em.isMergeCalled());
+
+		em.resetMockFlags();
+
+		entityMock = new MockingEntity(10);
+		mockDAO.persist(entityMock);
+		Assert.assertTrue(em.isMergeCalled());
+		Assert.assertFalse(em.isPersistCalled());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void removeTest() {
+		MockingEntity entityMock = new MockingEntity();
+		mockDAO.remove(entityMock);
+
+		entityMock = new MockingEntity(999);
+		mockDAO.remove(entityMock);
+		Assert.assertTrue(em.isRemoveCalled());
+	}
+
+	@Test
+	public void findByIDTest() {
+		mockDAO.findById(999);
+		Assert.assertTrue(em.isFindCalled());
+	}
 
 }
